0f6acefe6856797b57e5b20c1efce632acd377d4
Kushal Ghimire <kushal.upr@gmail.com>
2026-07-07 09:01:22 +0545

    add the login and register  route

--- jobportal/jobportal/urls.py
+++ jobportal/jobportal/urls.py
@@ -1,30 +1,14 @@
 """
 URL configuration for jobportal project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from portal.models import Company, Job
 from django.shortcuts import render
-
-from django.http import HttpResponse
 
 
 def admin2(request):
     companys = Company.objects.all()  # select * from company
-    # return HttpResponse("hello")
     return render(request, "admin2.html", {"companys": companys})
 
 def jobs(request):
@@ -35,5 +19,6 @@ def jobs(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("admin2/", admin2),
-    path("jobs/", jobs),
+    path("jobs/", jobs, name="jobs"),
+    path("", include("portal.urls")),   # login / register / logout
 ]
