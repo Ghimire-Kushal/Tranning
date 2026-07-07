1ec3c8bd069f547bf0b6566f4adbb5e075e3ec9c
Kushal Ghimire <kushal.upr@gmail.com>
2026-07-07 09:13:00 +0545

    aadd the dynamic routing login and rgisterr

--- jobportal/jobportal/urls.py
+++ jobportal/jobportal/urls.py
@@ -1,18 +1,22 @@
-"""
-URL configuration for jobportal project.
-"""
 from django.contrib import admin
 from django.urls import path, include
 from portal.models import Company, Job
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url="login")
+def jobs(request):
+    jobs = Job.objects.all()
+    return render(request, "jobs.html", {"jobs": jobs})
 
 
 def admin2(request):
-    companys = Company.objects.all()  # select * from company
+    companys = Company.objects.all()
     return render(request, "admin2.html", {"companys": companys})
 
+@login_required(login_url="login")
 def jobs(request):
-    jobs = Job.objects.all()   # select * from job
+    jobs = Job.objects.all()
     return render(request, "jobs.html", {"jobs": jobs})
 
 
@@ -20,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("admin2/", admin2),
     path("jobs/", jobs, name="jobs"),
-    path("", include("portal.urls")),   # login / register / logout
+    path("", include("portal.urls")),
 ]
