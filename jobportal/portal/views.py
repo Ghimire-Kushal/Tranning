c3c83cefbd09fd2bdb5c12325ceed445776e0ee6
Kushal Ghimire <kushal.upr@gmail.com>
2026-07-10 12:50:44 +0545

    add the click

--- jobportal/portal/views.py
+++ jobportal/portal/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404
-from portal.models import Job
+from .models import Job
+
 
 def register_view(request):
     if request.method == "POST":
@@ -34,7 +34,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect("jobs")   # redirect where you like after login
+            return redirect("jobs")
         messages.error(request, "Invalid username or password")
         return redirect("login")
 
@@ -44,6 +44,7 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
+
 
 def job_detail(request, job_id):
     job = get_object_or_404(Job, id=job_id)
