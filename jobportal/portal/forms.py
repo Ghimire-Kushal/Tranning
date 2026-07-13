bcd79e53efe9220d7b8e405bf900412407d27a89
Kushal Ghimire <kushal.upr@gmail.com>
2026-07-13 12:59:16 +0545

    push

--- jobportal/portal/forms.py
+++ jobportal/portal/forms.py
@@ -7,12 +7,8 @@ class ApplicationForm(forms.ModelForm):
         fields = ["full_name", "email", "phone", "cover_letter", "resume"]
         widgets = {
             "cover_letter": forms.Textarea(attrs={"rows": 6, "placeholder": "Why are you a good fit?"}),
-            "full_name": forms.TextInput(attrs={"placeholder": "Your full name"}),
-            "email": forms.EmailInput(attrs={"placeholder": "you@example.com"}),
-            "phone": forms.TextInput(attrs={"placeholder": "+977 98..."}),
+            "full_name": forms.TextInput(attrs={"placeholder": "Enter your Full_Name "}),
+            "email": forms.EmailInput(attrs={"placeholder": "Enter Your Email"}),
+            "phone": forms.TextInput(attrs={"placeholder": "Enter Your Number"}),
         }
-# class UserRegisterForm(forms.form):
-#     username = forms.TextInput()
-#     email = forms.EmailInput()
-#     password = forms.PasswordInput()
     
